656341c25defef877ddf72384819690d2bbff6ed
COPSTA <tutomx16@gmail.com>
2024-05-02 16:58:20 -0500

    Agrega variable necesarias para capturrar el endpoint y validar

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-<?php 
+<?php
 require_once  "controller/route.controller.php"; //Requiere el archivo y si no esta incluido lo vuelve a requerir
 
 
 $route = new ControllerRoutes(); //Crea la intancia de la clase 
 $route->index();
-
-?>
--- route/route.php
+++ route/route.php
@@ -1,30 +1,20 @@
-<?php 
-$json = array(
- "detalle"=>"no encontrado"
-);
+<?php
+
+$arrayRoute = explode("/", $_SERVER['REQUEST_URI']); //La variable $_SERVER['REQUEST_URI'] devuelve la url y el metodo explote divide una cadena en este caso la url por el delimitador "/" y la guarda en un array 
+
+echo "<pre>";
+print_r($arrayRoute);
+echo "<pre>";
 
 
-echo json_encode($json,true);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+if (count(array_filter($arrayRoute)) == 2) { //count devuelve el tamaño de la matriz, array_filter filtra el array bajo un criterio y devuelve el resultado por defecto remueve los elementos vacios
+    $json = array(
+        "detalle" => "no encontrado"
+    );
+    echo json_encode($json, true);
+    return;
+} else {
+    if (count(array_filter($arrayRoute)) == 3) {
+        $json = array("detalle" => "Estas en la vista cursos");
+    }
+}
