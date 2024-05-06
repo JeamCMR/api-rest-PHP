387714a82ef19d19435700e47ce0a422167a9ca0
COPSTA <tutomx16@gmail.com>
2024-05-06 14:12:23 -0500

    Agrega Funcion create,show,update cursos, con su respetivas peticiones

--- controller/cursos.controller.php
+++ controller/cursos.controller.php
@@ -7,7 +7,29 @@ class CursosController
     {
         //Cuando se hace  peticion a la api muestra de cursos, muestra este resultado
         $json = array(
-            "detalle" => "Estas en la vista cursos"
+            "detalle" => "Estas en la vista  curso"
+        );
+        echo json_encode($json, true);
+        return;
+    }
+    public function create(){
+        $json = array(
+            "detalle" => "Estas en la vista create cursos"
+        );
+        echo json_encode($json, true);
+        return;
+    }
+
+    public function show($id){
+        $json = array(
+            "detalle" => "Este es el curso con el id: ". " ". $id
+        );
+        echo json_encode($json, true);
+        return;
+    }
+    public function update($id){
+        $json = array(
+            "detalle" => "El curso actualizado con el id: ".$id ." Fue actualizado"
         );
         echo json_encode($json, true);
         return;
--- route/route.php
+++ route/route.php
@@ -18,20 +18,48 @@ if (count(array_filter($arrayRoute)) == 2) { //count devuelve el tamaño de la m
     if (count(array_filter($arrayRoute)) == 3) {
         //Cuando se pasa algun index a la url al $arrayRutas
         if(array_filter($arrayRoute)[3] == "cursos"){
-            if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "POST"){ //$_SERVER['REQUEST_METHOD'] Valida que tipo de motodo para consumir la api se usa
+            if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "POST")//$_SERVER['REQUEST_METHOD'] Valida que tipo de motodo para consumir la api se usa
+            { 
                 $cursos = new CursosController();
-                $cursos->index();
+                $cursos->create();
             } 
+            else if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "GET")
+            {
+                $cursos = new CursosController();
+                $cursos->index();   
+            }
            
         }
+
+
         if(array_filter($arrayRoute)[3] == "registro"){
-             //Cuando se hace  peticion a la api muestra de registro, muestra este resultado
-             if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "GET")
-             {
+            //Cuando se hace  peticion a la api muestra de registro, muestra este resultado
+            if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "GET")
+            {
                 $clientes = new ClientesController();
                 $clientes -> create();
-             }
+            }
+             
           
+        }
+
+    }else 
+    {
+        if(array_filter($arrayRoute)[3] == "cursos" && is_numeric(array_filter($arrayRoute)[4]) )
+        {
+            /**Realiza un peticion get para traer devuelta el curso GET */
+            if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "GET")
+            {
+                $curso = new CursosController();
+                $curso -> show(array_filter($arrayRoute)[4]);
+            }
+
+            /**Realiza un peticion get para traer devuelta el curso PUT */
+            if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "PUT")
+            {
+                $editarCurso = new CursosController();
+                $editarCurso -> update(array_filter($arrayRoute)[4]);
+            }
         }
     }
 }
